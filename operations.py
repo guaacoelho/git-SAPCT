53cdee81a5352bdb53a7bbb84e335a0efcd3e32c
Gustavo Coelho <coelho1914@gmail.com>
2025-05-23 15:36:30 -0300

    Add comentarios

--- operations.py
+++ operations.py
@@ -4,12 +4,14 @@ def soma(a, b):
 def subtracao(a, b):
     return a - b
 
+# ola tudo bem?
 def potencia(a, pot):
     return a ** pot
 
 def multiplicacao(a, b):
     return a * b
 
+# aqui a gente divide
 def divisao(a, b):
     if b == 0:
         raise Exception("Não pode dividir por zero")
